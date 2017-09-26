51e02505e6142250679f91992e8353bdccbe0803
DiogenestheDog <rguthrie14@gmail.com>
2017-09-25 17:31:20 -0700

    cleanup

--- serveMe.js
+++ serveMe.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
 //const port = process.argv[2] || 3000;
-const port = process.env.PORT || port
+const port = 3000;
 let server = http.createServer(function(req, res) {
   res.writeHead(200, {"Content-Type": "text/html"});
   resObj = {"ipaddress" : req.connection.remoteAddress, "language" : req.headers['accept-language'], "software" : req.headers['user-agent']};
   res.end(JSON.stringify(resObj));
 });
-server.listen(port);
+server.listen(process.env.PORT || port);
 console.log("listening on port " + port);
